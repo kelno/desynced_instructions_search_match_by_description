77ce239a4a43428f4938d16866b06b9dd25b30db
Kelno <kelnowm@gmail.com>
2025-07-27 00:29:00 +0200

    Better logging

--- Program.lua
+++ Program.lua
@@ -16,20 +16,34 @@
 --   end
 -- end
 
+local logPrefix = "[mod:instructions_search_match_by_description] "
+
+local function modPrint(message)
+  print(logPrefix .. message)
+end
+
+local function modError(message)
+  print(logPrefix .. "Error: " .. message)
+end
+
+-- End helper functions
+
+
+
 local prog = UI.GetRegisteredLayoutClass("Program")
 
 if not prog then
-  error("Couldn't override Program — registered class not found")
+  modError("Couldn't override Program — registered class not found")
   return
 end
 
 if not type(prog.on_filter) == "function" then
-  error("Couldn't override Program:on_filter — not a function")
+  modError(logPrefix .. "Couldn't override Program:on_filter — not a function")
   return
 end
 
 if not type(prog.construct) == "construct" then
-  error("Couldn't override Program:construct — not a function")
+  modError(logPrefix .. "Couldn't override Program:construct — not a function")
   return
 end
 
@@ -72,7 +86,7 @@ prog.on_filter = function(self, search, filter, popup_toolbox)
     cat.icon = collapse and "icon_small_arrow_right" or "icon_small_arrow_down"
   end
 end
-print("Override on_filter successful")
+modPrint("Override on_filter successful")
 
 -- Original function:
 -- function Program:on_filter(search, filter, popup_toolbox)
